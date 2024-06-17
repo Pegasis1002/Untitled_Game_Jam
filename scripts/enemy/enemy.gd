9db827793692c28fe3fd8e2283311e40fad94e10
Pegasis1002 <thewebserfer1234@gmail.com>
2024-06-17 21:24:32 +0530

    remade enemy, improved player

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,53 +1,76 @@
 extends CharacterBody2D
+
+@onready var timer = $Timer
+@export var patrol_points : Node
 @onready var animated_sprite = $AnimatedSprite2D
+@export var SPEED = 3000
 
+var Health : int = 100
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+enum state { Idle, Run, attack }
+var current_state : state
+var direction : Vector2 = Vector2.LEFT
+var number_of_points : int
+var point_pos : Array[Vector2]
+var current_point : Vector2
+var current_point_pos : int
+var can_walk : bool
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction = Vector2.LEFT
-
-var agro = false
+func _ready():
+	if patrol_points != null:
+		number_of_points = patrol_points.get_children().size()
+		for point in patrol_points.get_children():
+			point_pos.append(point.global_position)
+		current_point = point_pos[current_point_pos]
+	else:
+		print("No patrol points")
+	
+	current_state = state.Idle
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	if !agro:
-		animated_sprite.play("run")
-		if direction:
-			velocity.x = direction.x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		move_and_slide()
-
-
-func _on_startpoint_body_entered(body):
-	direction = Vector2.RIGHT
-	animated_sprite.flip_h = false
-
-func _on_end_point_body_entered(body):
-	direction = Vector2.LEFT
-	animated_sprite.flip_h = true
-
-
-func _on_agro_radius_body_entered(body):
-	agro = true
-	var ppos = (body.position - position).normalized()
-	print(ppos)
-	velocity.x = -direction.x * SPEED
+	var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	velocity.y += gravity * delta
+	enemy_idle(delta)
+	enemy_run(delta)
+	enemy_animations()
+	
 	move_and_slide()
 
-func _on_agro_radius_body_exited(body):
-	agro = false
+func enemy_idle(delta : float):
+	if !can_walk:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		current_state = state.Idle
+
+func enemy_run(delta : float):
+	if !can_walk:
+		return
 	
+	if abs(position.x - current_point.x) > 0.5:
+		velocity.x = direction.x * SPEED * delta
+		current_state = state.Run
+	else:
+		current_point_pos += 1
+		
+		if current_point_pos >= number_of_points:
+			current_point_pos = 0
+		
+		current_point = point_pos[current_point_pos]
+		velocity.x = direction.x * SPEED * delta
+		if current_point.x > position.x:
+			direction = Vector2.RIGHT
+		else:
+			direction = Vector2.LEFT
+		
+		can_walk = false
+		timer.start()
+	
+	animated_sprite.flip_h = direction.x < 0
 
-func _on_melle_radius_body_entered(body):
-	agro = true
-	attack(body)
+func enemy_animations():
+	if current_state == state.Idle && !can_walk:
+		animated_sprite.play("idle")
+	elif current_state == state.Run && can_walk:
+		animated_sprite.play("run")
 
-func attack(body):
-	animated_sprite.play("attack1")
+func _on_timer_timeout():
+	can_walk = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var startpoint = $"../Enemy/startpoint"
-@onready var end_point = $"../Enemy/end point"
 
 const SPEED = 230.0
 const JUMP_VELOCITY = -300.0
 const MAX_JUMP_TIME = 0.3  # Maximum time since last on floor to allow jumping
+
+enum State { idle, run, atack }
+var current_state : State
+var can_move : bool
+var is_attacking : bool
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Timer to keep track of how long it's been since the player was last on the floor.
 var time_since_on_floor = 0.0
+# Get the input direction: -1 0 +1
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -25,10 +29,39 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and time_since_on_floor <= MAX_JUMP_TIME:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction: -1 0 +1
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	
+	player_idle(delta)
+	player_run(delta, direction)
+	attack_system(delta)
+	player_animations(direction)
+
+	move_and_slide()
+
+func player_idle(delta : float):
+	if !can_move:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		current_state = State.idle
+
+func player_run(delta : float, direction):
+	if !can_move:
+		return
+	current_state = State.run
+	
+	if direction:
+		velocity.x = direction * SPEED
+	
+
+func attack_system(delta):
+	if Input.is_action_just_pressed("primary_attack"):
+		can_move = false
+		is_attacking = true
+	else:
+		can_move = true
+		is_attacking = false
+
+func player_animations(direction):
 	# Flip the sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -39,16 +72,11 @@ func _physics_process(delta):
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
-		else:
+		elif can_move:
 			animated_sprite.play("run")
+		elif is_attacking:
+			animated_sprite.play("attack1")
 	else:
 		animated_sprite.play("jumpp")
 	
-	# Update velocity based on direction
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
+	
